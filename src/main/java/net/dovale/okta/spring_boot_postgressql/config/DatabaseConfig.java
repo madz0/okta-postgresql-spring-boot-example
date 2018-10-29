88ec855c0a3242d449fae3e6be85b2da4b04bf28
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-28 23:42:29 -0300

    Configuration

---
+++ src/main/java/net/dovale/okta/spring_boot_postgressql/config/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package net.dovale.okta.spring_boot_postgressql.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class DatabaseConfig {
+}
