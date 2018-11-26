3173813967911a086a5a31bdcc02c4bcf63553fc
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-11-25 23:17:08 -0200

    Enable transaction management

--- src/main/java/net/dovale/okta/spring_boot_postgressql/config/DatabaseConfig.java
+++ src/main/java/net/dovale/okta/spring_boot_postgressql/config/DatabaseConfig.java
@@ -1,9 +1,9 @@
 package net.dovale.okta.spring_boot_postgressql.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories
+@EnableTransactionManagement
 public class DatabaseConfig {
 }
